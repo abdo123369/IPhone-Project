c4bf997c71a0a8d8c61032ac8e424ecb15e5bc2f
abdo123369 <147541105+abdo123369@users.noreply.github.com>
2023-10-22 01:23:50 +0300

    the finish

---
+++ main.js
@@ -0,0 +1,9 @@
+let img = document.querySelector('.img');
+let container = document.querySelector('.container'); 
+function phones(x){
+    img.src=x;
+}
+function colors(x){
+    container.style.background=x;
+
+}
